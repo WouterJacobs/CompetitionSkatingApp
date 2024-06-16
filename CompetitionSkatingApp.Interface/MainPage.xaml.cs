118f5e9ba9adba5908da418591becc8032043990
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-16 20:34:52 +0200

    Added icons and events to top buttons.

--- CompetitionSkatingApp.Interface/MainPage.xaml.cs
+++ CompetitionSkatingApp.Interface/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics;
+
 namespace CompetitionSkatingApp.Interface
 {
     public partial class MainPage : ContentPage
@@ -9,9 +11,30 @@ namespace CompetitionSkatingApp.Interface
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void newEventButton_Clicked(object sender, EventArgs e)
         {
-             DisplayAlert("New", "Making a new competition\nare you sure?", "Cancel");
+            DisplayAlert("Add new event", "Would you like to add a new event", "Yes", "No");
+        }
+
+        private void loadButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Load event", "Would you like to load an event", "Yes", "No");
+        }
+
+        private void saveButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Save event", "Would you like to save this event", "Yes", "No");
+        }
+
+        private void printButton_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Print results?", "Would you like print results", "Yes", "No");
+        }
+
+        private async void settingsButton_Clicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Settings?", "Would you like to go to settings", "Yes", "No");
+            Debug.WriteLine(answer);
         }
     }
 
