9ca6411082db2b664e71673f9f41eb3b1bb8abb2
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-16 19:41:04 +0200

    Removed the shell. Added a grid into main view. Made buttons attatched to the top with basic functionalities: New, Load,Save,Print,Settings.

--- CompetitionSkatingApp.Interface/MainPage.xaml.cs
+++ CompetitionSkatingApp.Interface/MainPage.xaml.cs
@@ -1,24 +1,17 @@
-﻿namespace CompetitionSkatingApp.Interface
+﻿
+namespace CompetitionSkatingApp.Interface
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+             DisplayAlert("New", "Making a new competition\nare you sure?", "Cancel");
         }
     }
 
