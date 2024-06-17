358ed9c553081834c2bc7ee8a0fe407d8acbaf01
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-17 14:30:14 +0200

    Back to regular clicked events for top bar

--- CompetitionSkatingApp.Interface/MainPage.xaml.cs
+++ CompetitionSkatingApp.Interface/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using CompetitionSkatingApp.Domain;
 using CompetitionSkatingApp.Interface.ViewModels;
 using System.Diagnostics;
 
@@ -7,7 +8,6 @@ namespace CompetitionSkatingApp.Interface
     public partial class MainPage : ContentPage
     {
         private readonly MainPageViewModel _mainPageViewModel;
-
         public MainPage(MainPageViewModel mainPageViewModel)
         {
             InitializeComponent();
@@ -15,24 +15,24 @@ namespace CompetitionSkatingApp.Interface
             BindingContext = _mainPageViewModel; // Set binding context here
         }
 
-        private void newEventButton_Clicked(object sender, EventArgs e)
+        private async void newEventButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Add new event", "Would you like to add a new event", "Yes", "No");
+            bool answer = await DisplayAlert("Add new event", "Would you like to add a new event", "Yes", "No");
         }
 
-        private void loadButton_Clicked(object sender, EventArgs e)
+        private async void loadButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Load event", "Would you like to load an event", "Yes", "No");
+            bool answer = await DisplayAlert("Load event", "Would you like to load an event", "Yes", "No");
         }
 
-        private void saveButton_Clicked(object sender, EventArgs e)
+        private async void saveButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Save event", "Would you like to save this event", "Yes", "No");
+            bool answer = await DisplayAlert("Save event", "Would you like to save this event", "Yes", "No");
         }
 
-        private void printButton_Clicked(object sender, EventArgs e)
+        private async void printButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Print results?", "Would you like print results", "Yes", "No");
+            bool answer = await DisplayAlert("Print results?", "Would you like print results", "Yes", "No");
         }
 
         private async void settingsButton_Clicked(object sender, EventArgs e)
--- CompetitionSkatingApp.Interface/ViewModels/MainPageViewModel.cs
+++ CompetitionSkatingApp.Interface/ViewModels/MainPageViewModel.cs
@@ -24,6 +24,7 @@ namespace CompetitionSkatingApp.Interface.ViewModels
             set { _hello = value; }
         }
 
+
     }
 }
 
