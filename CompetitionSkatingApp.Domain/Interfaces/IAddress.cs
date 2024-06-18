6c945e7017cf3230008811fe2a83c0825edde0fe
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-18 21:53:05 +0200

    Filled in IAddress

--- CompetitionSkatingApp.Domain/Interfaces/IAddress.cs
+++ CompetitionSkatingApp.Domain/Interfaces/IAddress.cs
@@ -2,5 +2,9 @@
 {
     public interface IAddress
     {
+        string City { get; set; }
+        string PostalCode { get; set; }
+        string Street { get; set; }
+        int Number { get; set; }
     }
 }
