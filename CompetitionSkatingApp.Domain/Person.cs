7830ca07361a0227e5818fb0fe7e20b9046ed356
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-25 13:39:45 +0200

    Implemented Person.cs

--- CompetitionSkatingApp.Domain/Person.cs
+++ CompetitionSkatingApp.Domain/Person.cs
@@ -9,7 +9,30 @@ namespace CompetitionSkatingApp.Domain
 {
     internal class Person : IPerson
     {
-        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private string _firstName;
+        private string _lastName;
+
+        internal Person(string firstName, string lastName)
+        {
+            _firstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            _lastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public override string ToString()
+        {
+            return $"FirstName: {_firstName}, LastName: {_lastName}";
+        }
     }
 }
