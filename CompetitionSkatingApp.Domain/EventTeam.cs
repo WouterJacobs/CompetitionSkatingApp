43f629232df26f24bed104216490b835c4755acb
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-25 13:38:09 +0200

    Implemented EventTeam.cs

--- CompetitionSkatingApp.Domain/EventTeam.cs
+++ CompetitionSkatingApp.Domain/EventTeam.cs
@@ -9,10 +9,61 @@ namespace CompetitionSkatingApp.Domain
 {
     internal class EventTeam : IEventTeam
     {
-        public IEventCoordinator Organizer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IEventCoordinator MasterOfCeremony { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IList<IOfficial> Chairmans { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IEventCoordinator Diskjockey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IList<IEventCoordinator> Scrutineers { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private IEventCoordinator _organizer;
+        private IEventCoordinator _masterOfCeremony;
+        private IList<IOfficial> _chairmans;
+        private IEventCoordinator _diskjockey;
+        private IList<IEventCoordinator> _scrutineers;
+
+        internal EventTeam(IEventCoordinator organizer, IEventCoordinator masterOfCeremony, IList<IOfficial> chairmans,
+            IEventCoordinator diskjockey, IList<IEventCoordinator> scrutineers)
+        {
+            _organizer = organizer ?? throw new ArgumentNullException(nameof(organizer));
+            _masterOfCeremony = masterOfCeremony ?? throw new ArgumentNullException(nameof(masterOfCeremony));
+            _diskjockey = diskjockey ?? throw new ArgumentNullException(nameof(diskjockey));
+            _chairmans = new List<IOfficial>();
+            _scrutineers = new List<IEventCoordinator>();
+        }
+
+        public IEventCoordinator Organizer
+        {
+            get => _organizer;
+            set => _organizer = value;
+        }
+
+        public IEventCoordinator MasterOfCeremony
+        {
+            get => _masterOfCeremony;
+            set => _masterOfCeremony = value;
+        }
+
+        public IList<IOfficial> Chairmans
+        {
+            get => _chairmans;
+            set => _chairmans = value;
+        }
+
+        public IEventCoordinator Diskjockey
+        {
+            get => _diskjockey;
+            set => _diskjockey = value;
+        }
+
+        public IList<IEventCoordinator> Scrutineers
+        {
+            get => _scrutineers;
+            set => _scrutineers = value;
+        }
+        
+        //TODO add a good tostring
+
+        public void AddChairman(IOfficial chairman)
+        {
+            _chairmans.Add(chairman);
+        }
+        public void AddScrutineer(IEventCoordinator scrutineer)
+        {
+            _scrutineers.Add(scrutineer);
+        }
     }
 }
