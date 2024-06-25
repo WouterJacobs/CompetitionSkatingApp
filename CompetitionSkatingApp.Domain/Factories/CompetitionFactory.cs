bd2caec8823ff964e9c3365272a8e780388ae7c8
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-25 14:17:06 +0200

    Made create method in CompetitionFactory.cs

--- CompetitionSkatingApp.Domain/Factories/CompetitionFactory.cs
+++ CompetitionSkatingApp.Domain/Factories/CompetitionFactory.cs
@@ -8,5 +8,11 @@ namespace CompetitionSkatingApp.Domain.Factories
 {
     public class CompetitionFactory
     {
+        static Competition CreateCompetition(string name, Discipline discipline, int amountOfDances,
+            AgeCategory ageCategory, Chairman chairperson)
+        {
+            Competition competition = new Competition(name, discipline, amountOfDances, ageCategory, chairperson);
+            return competition;
+        }
     }
 }
