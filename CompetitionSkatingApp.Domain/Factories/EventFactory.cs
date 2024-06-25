c477eca3dd476340e1fa700d8ace64af10db2ba6
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-25 14:26:07 +0200

    made dance factory

--- CompetitionSkatingApp.Domain/Factories/EventFactory.cs
+++ CompetitionSkatingApp.Domain/Factories/EventFactory.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CompetitionSkatingApp.Domain.Interfaces;
 
 namespace CompetitionSkatingApp.Domain.Factories
 {
     public class EventFactory
     {
+        public static IDancingEvent CreateEvent(string name, DateOnly startDate, DateOnly endDate, IEventLocation location, IEventSchool school,
+            string description, IEventTeam eventTeam)
+        {
+            DancingEvent dancingEvent = new DancingEvent(name, startDate, endDate, location, school, description, eventTeam);
+            return dancingEvent;
+        }
     }
 }
