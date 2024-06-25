a72d8b4423f1cdb3fce6a3ff3051be16fc36118d
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-25 13:27:10 +0200

    Implemented Couple.cs

--- CompetitionSkatingApp.Domain/Couple.cs
+++ CompetitionSkatingApp.Domain/Couple.cs
@@ -9,8 +9,38 @@ namespace CompetitionSkatingApp.Domain
 {
     internal class Couple : ICouple
     {
-        public IPerson Man { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IPerson Lady { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IClub Club { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private IPerson _man;
+        private IPerson _lady;
+        private IClub _club;
+
+        internal Couple(IPerson man, IPerson lady, IClub club)
+        {
+            _man = man ?? throw new ArgumentNullException(nameof(man));
+            _lady = lady ?? throw new ArgumentNullException(nameof(lady));
+            _club = club ?? throw new ArgumentNullException(nameof(club));
+        }
+
+        public IPerson Man
+        {
+            get => _man;
+            set => _man = value;
+        }
+
+        public IPerson Lady
+        {
+            get => _lady;
+            set => _lady = value;
+        }
+
+        public IClub Club
+        {
+            get => _club;
+            set => _club = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Man: {_man.FirstName} {_man.LastName}, Lady: {_lady.FirstName} {_lady.LastName}, Club: {_club.Name}";
+        }
     }
 }
