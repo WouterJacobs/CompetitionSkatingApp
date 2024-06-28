1e0d277eb26b664124b7835b70491676d4e974ee
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-06-29 01:15:47 +0200

    Added 2 classes in Domain.Util in preparation for reading and writing to file that contains the data of an event.

---
+++ CompetitionSkatingApp.Infrastructure/SkatingAppDbContext.cs
@@ -0,0 +1,35 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using CompetitionSkatingApp.Domain.Interfaces;
+//using CompetitionSkatingApp.Domain.Factories;
+//using CompetitionSkatingApp.Domain;
+
+//namespace CompetitionSkatingApp.Infrastructure
+//{
+//    public class SkatingAppDbContext : DbContext
+//    {
+//        public DbSet<Competition> Competitions { get; set; }
+
+//        public SkatingAppDbContext(DbContextOptions options) : base(options)
+//        {
+//        }
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            modelBuilder.Entity<Competition>().HasData(SeedCompetitions());
+
+//        }
+//        private List<ICompetition> SeedCompetitions()
+//        {
+//            CompetitionFactory competitionFactory = new CompetitionFactory();
+//            List<ICompetition> competitions = new List<ICompetition>(){
+//                competitionFactory.CreateCompetition("Adult Masterclass latin", Domain.Discipline.LATIN, 5, Domain.AgeCategory.ADULT, null),
+//                competitionFactory.CreateCompetition("Adult Amateur Ballroom", Domain.Discipline.STANDARD, 4, Domain.AgeCategory.ADULT, null),
+//            };
+//            return competitions;
+//        }
+//    }
+//}
